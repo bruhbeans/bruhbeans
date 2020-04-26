88f345a1c7fbcda0366ea632911510fff0db932e
bruhbeans <point.less@outlook.com>
2020-04-26 03:17:16 +0100

    token working

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from discord.ext import commands
-
 bot = commands.Bot(command_prefix='-', help_command=None)
 
 @bot.event
 async def on_ready():
     print('login')
 
-bot.run(os.getenv('TOKEN'))
+bot.run(os.getenv("BOT_TOKEN"))
